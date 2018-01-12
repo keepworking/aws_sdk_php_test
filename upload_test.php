f98fff2600cfb2350c91d65abe8935775e41346c
yevgeny hong <hongyevgeny@gmail.com>
2018-01-12 15:03:51 +0900

    Add files via upload

---
+++ upload_test.php
@@ -0,0 +1,43 @@
+<?php
+require '../lib/aws/aws-autoloader.php';
+
+// $s3 = new Aws\S3\S3Client([
+//   'version' => 'lastest',
+//   'region' => 'us-east-1'
+// ]);
+
+
+$s3Client = new Aws\S3\S3Client([
+  'version'     => 'latest',
+  'region'      => 'ap-northeast-2',
+  'credentials' => [
+    'key'    => '****************',
+    'secret' => '****************',
+  ],
+]);
+
+if($_FILES["file"] != null){
+  try {
+    $result = $s3Client->putObject([
+      'Bucket' => '***********',
+      'Key'    => $_FILES["file"]['name'],
+      'Body'   => fopen($_FILES["file"]["tmp_name"],'r'),
+      'ACL'    => 'public-read',
+    ]);
+
+    ?>
+    <img src="<?=$result['ObjectURL'];?>">
+
+    <?php
+  } catch (Aws\S3\Exception\S3Exception $e) {
+    echo "There was an error uploading the file.\n";
+  }
+}
+
+?>
+
+
+<form class="" action="upload.php" enctype="multipart/form-data" method="post">
+  <input type="file" name="file" value="">
+  <input type="submit" name="" value="">
+</form>
